4b71e29e8755485fb564ba852e6e26b56c6ff8e9
MU-Software <musoftware@daum.net>
2023-11-27 00:43:40 +0900

    Add cookie key to UserJWTTokenTypeSetting

--- app/const/jwt.py
+++ app/const/jwt.py
@@ -2,16 +2,20 @@ import dataclasses
 import datetime
 import enum
 
+import app.const.cookie as cookie_const
+
 
 class UserJWTTokenType(enum.Enum):
     @dataclasses.dataclass(frozen=True)
     class UserJWTTokenTypeSetting:
         refresh_delta: datetime.timedelta
         expiration_delta: datetime.timedelta
+        cookie_key: cookie_const.CookieKey | None = None
 
     refresh = UserJWTTokenTypeSetting(
         refresh_delta=datetime.timedelta(days=6),
         expiration_delta=datetime.timedelta(days=7),
+        cookie_key=cookie_const.CookieKey.REFRESH_TOKEN,
     )
     access = UserJWTTokenTypeSetting(
         refresh_delta=datetime.timedelta(minutes=15),
