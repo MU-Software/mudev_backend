23ca7df2d21ba3ce234449a0a99c34dd6272aa75
MU-Software <musoftware@daum.net>
2023-12-09 22:21:23 +0900

    Update OpenAPITag enum values

--- app/const/tag.py
+++ app/const/tag.py
@@ -1,10 +1,17 @@
+from __future__ import annotations
+
 import enum
 
 
 class OpenAPITag(enum.StrEnum):
+    def _generate_next_value_(  # type: ignore[override]
+        name: str, start: int, count: int, last_values: list[str]
+    ) -> str:
+        return " ".join(map(lambda x: x.capitalize(), name.split("_")))
+
     HEALTH_CHECK = enum.auto()
     USER = enum.auto()
     USER_FILE = enum.auto()
     USER_SIGNIN_HISTORY = enum.auto()
 
-    YTDL = enum.auto()
+    YTDL = "YouTube Downloader"
