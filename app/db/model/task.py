c00b6801fe0c4ab78bb7cf67766c086026ac77e8
MU-Software <musoftware@daum.net>
2023-12-09 20:47:22 +0900

    Add state column to Task model

--- app/db/model/task.py
+++ app/db/model/task.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 
+import app.const.celery as celery_const
 import app.db.__mixin__ as db_mixin
 import app.db.__type__ as db_types
 
@@ -12,6 +13,8 @@ class Task(db_mixin.DefaultModelMixin):
     args: sa_orm.Mapped[db_types.Json_Nullable]
     kwargs: sa_orm.Mapped[db_types.Json_Nullable]
     startable: sa_orm.Mapped[db_types.Bool_DTrue]
-    done: sa_orm.Mapped[db_types.Bool_DFalse]
+    state: sa_orm.Mapped[celery_const.CeleryTaskStatus] = sa_orm.mapped_column(
+        sa.Enum(celery_const.CeleryTaskStatus, native_enum=False), nullable=False, index=True
+    )
 
     created_by: sa_orm.Mapped[db_types.UserFK_Nullable]
