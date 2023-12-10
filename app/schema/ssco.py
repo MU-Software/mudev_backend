0e9fe7e479e2604b65984ea1edffca41c88077fb
MU-Software <musoftware@daum.net>
2023-12-10 23:33:31 +0900

    Update VideoDTO and VideoCreate models

--- app/schema/ssco.py
+++ app/schema/ssco.py
@@ -1,14 +1,58 @@
+from __future__ import annotations
+
+import json
+import typing
+import uuid
+
 import pydantic
+
+import app.util.ext_api.youtube as youtube_util
+
+
+class VideoDTO(pydantic.BaseModel):
+    youtube_vid: str
+    title: str
+    thumbnail_uuid: uuid.UUID
+    data: pydantic.Json | None = None
+
+
+class VideoDownloadRequestPayload(pydantic.BaseModel):
+    url: pydantic.AnyHttpUrl = pydantic.Field(exclude=True)
+
+    @pydantic.field_validator("url", mode="before")
+    @classmethod
+    def validate_url(cls, value: pydantic.AnyHttpUrl) -> str:
+        if not youtube_util.extract_vid_from_url(str(value)):
+            raise ValueError("invalid youtube url")
+        return value
+
+    @pydantic.computed_field  # type: ignore[misc]
+    @property
+    def youtube_vid(self) -> str:
+        return youtube_util.extract_vid_from_url(str(self.url))
 
 
 class VideoCreate(pydantic.BaseModel):
     youtube_vid: str
-    title: str
+    title: str | None = None
+    thumbnail_uuid: uuid.UUID | None = None
     data: pydantic.Json | None = None
+
+    __primary_fields__: typing.ClassVar[set[str]] = {"youtube_vid"}
+
+    @pydantic.field_serializer("data")
+    def serialize_data(self, v: pydantic.Json | None) -> str | None:
+        return json.loads(v) if v else None
 
 
 class VideoUpdate(pydantic.BaseModel):
-    ...
+    title: str
+    thumbnail_uuid: uuid.UUID
+    data: pydantic.Json | None = None
+
+    @pydantic.field_serializer("data")
+    def serialize_data(self, v: pydantic.Json | None) -> str | None:
+        return json.loads(v) if v else None
 
 
 class PlaylistCreate(pydantic.BaseModel):
