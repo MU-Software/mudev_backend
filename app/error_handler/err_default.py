4252743c016795b3a18e336ae2d2343b97d6920e
MU-Software <musoftware@daum.net>
2024-01-27 21:41:37 +0900

    Add error handler for JSONDecodeError and update normalizer in pydantic

--- app/error_handler/err_default.py
+++ app/error_handler/err_default.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import json
+
 import pydantic_core
 
 import app.const.error as error_const
@@ -10,11 +12,21 @@ def valueerror_handler(req: err_type.ReqType, err: ValueError) -> err_type.RespT
     return error_const.ErrorStruct.from_exception(err).response()
 
 
+def jsondecodeerror_handler(req: err_type.ReqType, err: json.JSONDecodeError) -> err_type.RespType:
+    return error_const.ErrorStruct(
+        type="json_decode_error",
+        msg="이해할 수 없는 유형의 데이터를 받았어요.",
+        input=err.doc,
+        ctx={"pos": err.pos, "lineno": err.lineno, "colno": err.colno, "msg": err.msg},
+    ).response()
+
+
 def exception_handler(req: err_type.ReqType, err: pydantic_core.ValidationError) -> err_type.RespType:
     return error_const.ServerError.UNKNOWN_SERVER_ERROR().response()
 
 
 error_handler_patterns = {
     ValueError: valueerror_handler,
+    json.JSONDecodeError: jsondecodeerror_handler,
     Exception: exception_handler,
 }
--- app/util/pydantic/normalizer.py
+++ app/util/pydantic/normalizer.py
@@ -1,3 +1,4 @@
+import json
 import typing
 
 import pydantic
@@ -7,7 +8,10 @@ import app.util.mu_string as mu_string
 
 class NormalizerModelMixin(pydantic.BaseModel):
     @pydantic.model_validator(mode="before")
-    def normalizer(cls, values: dict[str, typing.Any]) -> dict[str, typing.Any]:
-        for k, v in values.items():
-            values[k] = mu_string.normalize(v).strip() if isinstance(v, str) else v
-        return values
+    def normalizer(cls, values: dict[str, typing.Any] | str | bytes) -> dict[str, typing.Any]:
+        if not isinstance(parsed_values := values if isinstance(values, dict) else json.loads(values), dict):
+            raise ValueError(f"이해할 수 없는 유형의 데이터를 받았어요. [NotDict|{type(values)}]")
+
+        for k, v in parsed_values.items():
+            parsed_values[k] = mu_string.normalize(v).strip() if isinstance(v, str) else v
+        return parsed_values
