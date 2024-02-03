1b49394baed15e7b28e95f255a64f3e0b7eee353
MU-Software <musoftware@daum.net>
2024-02-04 03:31:39 +0900

    Add fastapi_http_exception_handler to error_handler_patterns

--- app/error_handler/err_starlette.py
+++ app/error_handler/err_starlette.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import fastapi.exception_handlers
 import starlette.exceptions
 
 import app.const.error as error_const
@@ -7,8 +8,16 @@ import app.error_handler.__type__ as err_type
 import app.util.mu_string as string_util
 
 
+async def fastapi_http_exception_handler(
+    req: err_type.ReqType,
+    err: fastapi.exceptions.HTTPException,
+) -> err_type.RespType:
+    return await fastapi.exception_handlers.http_exception_handler(req, err)
+
+
 def starlette_http_exception_handler(
-    req: err_type.ReqType, err: starlette.exceptions.HTTPException
+    req: err_type.ReqType,
+    err: starlette.exceptions.HTTPException,
 ) -> err_type.RespType:
     response = error_const.ErrorStruct(
         status_code=err.status_code,
@@ -20,4 +29,7 @@ def starlette_http_exception_handler(
     return response
 
 
-error_handler_patterns = {starlette.exceptions.HTTPException: starlette_http_exception_handler}
+error_handler_patterns = {
+    fastapi.exceptions.HTTPException: fastapi_http_exception_handler,
+    starlette.exceptions.HTTPException: starlette_http_exception_handler,
+}
