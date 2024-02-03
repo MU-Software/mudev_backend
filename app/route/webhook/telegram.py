d68514e93cd8c912ac065ab353dc932b210f388c
MU-Software <musoftware@daum.net>
2024-02-04 01:05:35 +0900

    Refactor Telegram API handling

--- app/route/webhook/telegram.py
+++ app/route/webhook/telegram.py
@@ -1,113 +1,140 @@
 import logging
-import uuid
+import re
+import typing
 
 import fastapi
-import redis
-import sqlalchemy.ext.asyncio as sa_ext_asyncio
 import telegram
 
 import app.celery_task.task.ytdl as ytdl_task
-import app.config.fastapi as fastapi_config
+import app.const.error as error_const
 import app.const.sns as sns_const
 import app.const.tag as tag_const
 import app.crud.ssco as ssco_crud
 import app.crud.user as user_crud
+import app.dependency.common as common_dep
 import app.schema.ssco as ssco_schema
 import app.schema.user as user_schema
 import app.util.ext_api.telegram as telegram_util
 import app.util.ext_api.youtube as youtube_util
+import app.util.fastapi as fastapi_util
+import app.util.mu_string as string_util
 
 logger = logging.getLogger(__name__)
 router = fastapi.APIRouter(prefix="/webhook/telegram", tags=[tag_const.OpenAPITag.YTDL])
 
 
-async def start(
-    request: fastapi.Request,
-    update: telegram.Update,
-    config_obj: fastapi_config.FastAPISetting,
-    db_session: sa_ext_asyncio.AsyncSession,
-    redis_session: redis.Redis,
-    user_uuid: uuid.UUID | None,
-) -> None:
-    await update.effective_message.reply_text(
-        "본 봇은 먼저 mudev.cc의 계정과 연동한 후 사용하실 수 있습니다.\n/auth 을 누르거나 입력하셔서 인증을 진행해주세요."
-    )
+async def send_help(ctx: telegram_util.CommandHandlerContext) -> None:
+    help_text = "\n".join(f"{h.title}[{h.pattern}] : {h.description}" for h in ctx.handlers.values() if h.show_in_help)
+    await ctx.payload.effective_message.reply_text(text=help_text)
 
 
-async def auth_user(
-    request: fastapi.Request,
-    update: telegram.Update,
-    config_obj: fastapi_config.FastAPISetting,
-    db_session: sa_ext_asyncio.AsyncSession,
-    redis_session: redis.Redis,
-    user_uuid: uuid.UUID | None,
-) -> None:
-    if user_uuid:
-        await update.effective_message.reply_text("이미 연동된 계정이 있습니다.")
-        raise fastapi.HTTPException(status_code=409, detail="이미 연동된 계정이 있습니다.")
-    if not (sns_user := update.effective_user):
-        raise fastapi.HTTPException(status_code=422, detail="유저 정보를 얻을 수 없었습니다.")
-    if not (sns_msg := update.effective_message):
-        raise fastapi.HTTPException(status_code=422, detail="메시지에서 정보를 얻을 수 없었습니다.")
-    sns_chat = sns_msg.chat
+async def start(ctx: telegram_util.CommandHandlerContext) -> None:
+    msg = "본 봇은 먼저 mudev.cc의 계정과 연동한 후 사용하실 수 있습니다.\n/auth 을 누르거나 입력하셔서 인증을 진행해주세요."
+    await ctx.payload.effective_message.reply_text(msg)
+
+
+async def auth_user(ctx: telegram_util.CommandHandlerContext) -> None:
+    if ctx.user_uuid:
+        telegram_util.send_msg_and_raise(ctx.payload, error_const.TelegramError.USER_ALREADY_SYNCED())
+
+    sns_user = typing.cast(telegram.User, ctx.payload.effective_user)
+    sns_chat = ctx.payload.effective_message.chat
 
     sns_type = sns_const.SNSAuthInfoUserAgentEnum.telegram.value
     sns_token = user_schema.SNSClientInfo(sns_type=sns_type, user_id=sns_user.id, chat_id=sns_chat).model_dump_json()
     sns_info = user_schema.SNSAuthInfo(user_agent=sns_type, client_token=sns_token)
 
-    key = config_obj.secret_key.get_secret_value()
-    url = config_obj.frontend_name + "/user/sns?sns_token=" + sns_info.to_token(key)
-    await update.effective_message.reply_text(
-        text="아래 버튼을 눌러 mudev.cc 계정 연동을 진행해주세요.",
-        reply_markup=telegram.InlineKeyboardMarkup([[telegram.InlineKeyboardButton(text="mudev.cc 인증", url=url)]]),
+    key = ctx.config.secret_key.get_secret_value()
+    url = ctx.config.project.frontend_domain + "/user/sns?sns_token=" + sns_info.to_token(key)
+    btn_markup = telegram.InlineKeyboardMarkup([[telegram.InlineKeyboardButton(text="mudev.cc 인증", url=url)]])
+    await ctx.payload.effective_message.reply_text(
+        text="아래 버튼을 눌러 mudev.cc 계정 연동을 진행해주세요.", reply_markup=btn_markup
     )
 
 
-async def create_ytdl_task(
-    request: fastapi.Request,
-    update: telegram.Update,
-    config_obj: fastapi_config.FastAPISetting,
-    db_session: sa_ext_asyncio.AsyncSession,
-    redis_session: redis.Redis,
-    user_uuid: uuid.UUID | None,
-) -> None:
-    if not (message := update.effective_message):
+async def create_ytdl_task(ctx: telegram_util.CommandHandlerContext) -> None:
+    if not (message := ctx.payload.effective_message):
         raise fastapi.HTTPException(status_code=422, detail="메시지에서 정보를 얻을 수 없었습니다.")
     if not (youtube_id := youtube_util.extract_vid_from_url(message.text)):
-        await update.effective_message.reply_text(text="유효한 YouTube URL이 아니에요.")
+        await ctx.payload.effective_message.reply_text(text="유효한 YouTube URL이 아니에요.")
         return None
 
     video_create_obj = ssco_schema.VideoCreate(youtube_vid=youtube_id)
-    video_record, created = await ssco_crud.videoCRUD.get_or_create_async(db_session, video_create_obj)
-    video_record.users.add(await user_crud.userCRUD.get(db_session, uuid=user_uuid))
-    await db_session.commit()
+    video_record, created = await ssco_crud.videoCRUD.get_or_create_async(ctx.db_session, video_create_obj)
+    video_record.users.add(await user_crud.userCRUD.get(ctx.db_session, uuid=ctx.user_uuid))
+    await ctx.db_session.commit()
 
     if created:
         ytdl_task.ytdl_downloader_task.delay(youtube_vid=youtube_id)
 
 
-telegram_util.register_telegram_webhook_handler(
-    router=router,
-    route="/ssco",
-    cmds={
-        "/start": telegram_util.CommandHandler(
-            pattern="/start",
-            title="start",
-            description="봇과의 채팅을 시작합니다.",
-            handler=start,
-        ),
-        "/auth": telegram_util.CommandHandler(
-            pattern="/auth",
-            title="계정 연동",
-            description="mudev.cc의 계정과 연동을 시작합니다.",
-            handler=auth_user,
-        ),
-        youtube_util.VIDEO_REGEX: telegram_util.CommandHandler(
-            pattern=youtube_util.VIDEO_REGEX,
-            title="유튜브 영상 다운로드",
-            description="유튜브 영상을 다운로드합니다.",
-            show_in_help=False,
-            handler=create_ytdl_task,
-        ),
-    },
-)
+cmds: dict[re.Pattern | str, telegram_util.CommandHandler] = {
+    "/start": telegram_util.CommandHandler(
+        pattern="/start",
+        title="start",
+        description="봇과의 채팅을 시작합니다.",
+        handler=start,
+    ),
+    "/auth": telegram_util.CommandHandler(
+        pattern="/auth",
+        title="계정 연동",
+        description="mudev.cc의 계정과 연동합니다.",
+        handler=auth_user,
+    ),
+    "/help": telegram_util.CommandHandler(
+        pattern="/help",
+        title="도움말",
+        description="사용 가능한 명령어를 보여줍니다.",
+        handler=send_help,
+        show_in_help=False,
+    ),
+    youtube_util.VIDEO_REGEX: telegram_util.CommandHandler(
+        pattern=youtube_util.VIDEO_REGEX,
+        title="유튜브 영상 다운로드",
+        description="유튜브 영상을 다운로드합니다.",
+        handler=create_ytdl_task,
+        show_in_help=False,
+    ),
+}
+
+
+@router.post("", response_model=fastapi_util.EmptyResponseSchema)
+async def webhook_handler(
+    request: fastapi.Request,
+    config_obj: common_dep.settingDI,
+    db_session: common_dep.dbDI,
+    redis_session: common_dep.redisDI,
+) -> dict[str, str]:
+    bot_token = config_obj.project.ssco.telegram_bot_token.get_secret_value()
+    bot = telegram.Bot(token=bot_token)
+
+    if not (payload := telegram_util.parse_request(await request.body(), bot)):
+        error_const.ClientError.REQUEST_BODY_EMPTY().raise_()
+    if not ((msg_obj := payload.effective_message) and (msg_str := msg_obj.text)):
+        telegram_util.send_msg_and_raise(payload, error_const.TelegramError.MESSAGE_NOT_GIVEN())
+    if not (user := payload.effective_user):
+        telegram_util.send_msg_and_raise(payload, error_const.TelegramError.USER_NOT_GIVEN())
+    if user.is_bot:
+        telegram_util.send_msg_and_raise(payload, error_const.AuthZError.BOT_USER_NOT_ALLOWED())
+
+    if not (handler := telegram_util.get_handler(cmds, string_util.normalize(msg_str).strip())):
+        telegram_util.send_msg_and_raise(payload, error_const.TelegramError.HANDLER_NOT_MATCH())
+
+    sns_type = sns_const.SNSAuthInfoUserAgentEnum.telegram
+    user_uuid = await user_crud.snsAuthInfoCRUD.sns_user_to_user(db_session, sns_type, user.id)
+
+    if handler.require_auth and not user_uuid:
+        telegram_util.send_msg_and_raise(payload, error_const.AuthZError.REQUIRES_ACCOUNT_SYNC())
+
+    await handler.handler(
+        telegram_util.CommandHandlerContext(
+            request=request,
+            payload=payload,
+            config=config_obj,
+            db_session=db_session,
+            redis_session=redis_session,
+            user_uuid=user_uuid,
+            handlers=cmds,
+        )
+    )
+    return {"message": "ok"}
--- app/util/ext_api/telegram.py
+++ app/util/ext_api/telegram.py
@@ -13,23 +13,18 @@ import sqlalchemy.ext.asyncio as sa_ext_asyncio
 import telegram
 
 import app.config.fastapi as fastapi_config
-import app.const.sns as sns_const
-import app.crud.user as user_crud
-import app.dependency.common as common_dep
-import app.util.fastapi as fastapi_util
-import app.util.mu_string as string_util
+import app.const.error as error_const
 
-CmdHandlerCallable = typing.Callable[
-    [
-        fastapi.Request,
-        telegram.Update,
-        fastapi_config.FastAPISetting,
-        sa_ext_asyncio.AsyncSession,
-        redis.Redis,
-        uuid.UUID | None,
-    ],
-    typing.Awaitable[None],
-]
+
+@dataclasses.dataclass
+class CommandHandlerContext:
+    request: fastapi.Request
+    payload: telegram.Update
+    config: fastapi_config.FastAPISetting
+    db_session: sa_ext_asyncio.AsyncSession
+    redis_session: redis.Redis
+    user_uuid: uuid.UUID | None
+    handlers: dict[re.Pattern | str, CommandHandler]
 
 
 @dataclasses.dataclass
@@ -37,34 +32,17 @@ class CommandHandler:
     pattern: re.Pattern | str
     title: str
     description: str
+    handler: typing.Callable[[CommandHandlerContext], typing.Awaitable[None]]
     require_auth: bool = False
-    handler: CmdHandlerCallable | None = None
     show_in_help: bool = True
 
 
-CmdHandlerMapType = dict[re.Pattern | str, CommandHandler]
-
-
-def add_help_command(cmds: CmdHandlerMapType) -> CmdHandlerMapType:
-    async def send_help_message(
-        request: fastapi.Request,
-        update: telegram.Update,
-        config_obj: fastapi_config.FastAPISetting,
-        db_session: sa_ext_asyncio.AsyncSession,
-        redis_session: redis.Redis,
-        user_uuid: uuid.UUID | None,
-    ) -> None:
-        help_text = "\n".join(f"{h.title}[{h.pattern}] : {h.description}" for h in cmds.values() if h.show_in_help)
-        await update.effective_message.reply_text(text=help_text)
-
-    cmd_helper = CommandHandler(
-        pattern="/help",
-        title="도움말",
-        description="사용 가능한 명령어를 보여줍니다.",
-        handler=send_help_message,
-        show_in_help=False,
-    )
-    return cmds | {"/help": cmd_helper}
+def send_msg_and_raise(update: telegram.Update, err: error_const.ErrorStruct) -> typing.NoReturn:
+    if update.effective_message:
+        update.effective_message.reply_text(err.msg)
+    elif update.effective_chat:
+        update.get_bot().send_message(chat_id=update.effective_chat.id, text=err.msg)
+    err.raise_()
 
 
 def is_handler_pattern_match(pattern: re.Pattern | str, in_str: str) -> bool:
@@ -73,57 +51,18 @@ def is_handler_pattern_match(pattern: re.Pattern | str, in_str: str) -> bool:
     )
 
 
+def get_handler(cmds: dict[re.Pattern | str, CommandHandler], in_str: str) -> CommandHandler | None:
+    for pattern, handler in cmds.items():
+        if is_handler_pattern_match(pattern, in_str):
+            return handler
+    return None
+
+
 def parse_request(request: str | bytes | dict | telegram.Update, bot: telegram.Bot | None) -> telegram.Update | None:
     if isinstance(request, telegram.Update):
         request.set_bot(bot)
         return request
 
     with contextlib.suppress(Exception):
-        if isinstance(request, (bytes, str)):
-            data: dict = json.loads(request)
-        return telegram.Update.de_json(data, bot)
+        return telegram.Update.de_json(json.loads(request) if isinstance(request, (bytes, str)) else request, bot)
     return None
-
-
-def register_telegram_webhook_handler(*, router: fastapi.APIRouter, route: str = "", cmds: CmdHandlerMapType) -> None:
-    cmds = add_help_command(cmds)
-
-    @router.post(route, response_model=fastapi_util.EmptyResponseSchema)
-    async def webhook_handler(
-        request: fastapi.Request,
-        config_obj: common_dep.settingDI,
-        db_session: common_dep.dbDI,
-        redis_session: common_dep.redisDI,
-    ) -> dict[str, str]:
-        bot_token = config_obj.project.ssco.telegram_bot_token.get_secret_value()
-        bot = telegram.Bot(token=bot_token)
-
-        if not (payload := parse_request(await request.body(), bot)):
-            raise fastapi.HTTPException(status_code=400, detail="읽은 메시지를 이해할 수 없어요.")
-
-        if not ((msg_obj := payload.effective_message) and (msg_str := msg_obj.text)):
-            await bot.send_message(chat_id=msg_obj.chat_id, text="메시지를 읽었지만, 할 수 있는 일이 적혀있지 않았어요.")
-            raise fastapi.HTTPException(status_code=400, detail="메시지를 읽었지만, 할 수 있는 일이 적혀있지 않았어요.")
-
-        msg_str = string_util.normalize(msg_str).strip()
-        for pattern, handler in cmds.items():
-            if is_handler_pattern_match(pattern, msg_str):
-                if (user := payload.effective_user) and user.is_bot:
-                    error_msg = "해당 사용자는 이 기능을 사용할 수 없습니다."
-                    raise fastapi.HTTPException(status_code=401, detail=error_msg)
-
-                user_uuid = await user_crud.snsAuthInfoCRUD.sns_user_to_user(
-                    db_session,
-                    sns_const.SNSAuthInfoUserAgentEnum.telegram,
-                    user.id if user else None,
-                )
-
-                if handler.require_auth and not user_uuid:
-                    error_msg = "기능을 사용하기 위해서는 먼저 mudev.cc의 계정과 연동을 해야합니다."
-                    raise fastapi.HTTPException(status_code=403, detail=error_msg)
-
-                await handler.handler(request, payload, config_obj, db_session, redis_session, user_uuid)
-                return {"message": "ok"}
-
-        await payload.effective_message.reply_text("무슨 말씀이신지 이해하지 못했어요, 다시 입력해주세요.")
-        return {"message": "ok"}
