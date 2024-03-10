adf3373fa4f88d02035d46e7ea88f41a8ff06c2a
MU-Software <musoftware@daum.net>
2024-03-10 17:57:59 +0900

    Remove ytdl_updater task

--- app/celery_task/task/__init__.py
+++ app/celery_task/task/__init__.py
@@ -1,7 +1,3 @@
 import app.celery_task.task.ytdl as ytdl_task
-import app.celery_task.task.ytdl_updater as ytdl_updater_task
 
-__all__ = [
-    "ytdl_task",
-    "ytdl_updater_task",
-]
+__all__ = ["ytdl_task"]
